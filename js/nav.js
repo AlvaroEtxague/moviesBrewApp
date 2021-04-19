c00a724adfd3efa99fd6bb702d1f2ec31627c153
Tinowski <alvaroetxague@gmail.com>
2021-04-19 23:37:48 +0100

    Adding nav.js and script.js

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+function openNav() {
+	document.getElementById('mySidenav').style.width = '50%';
+
+	document.getElementById('backdrop').style.display = 'block';
+}
+
+function closeNav() {
+	document.getElementById('mySidenav').style.width = '0';
+
+	document.getElementById('backdrop').style.display = 'none';
+}
